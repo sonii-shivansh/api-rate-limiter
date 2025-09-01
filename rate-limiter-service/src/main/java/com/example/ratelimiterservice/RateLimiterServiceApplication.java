ec129a0633213dd9fcda1948f1dd31c14d0a9729
Shivansh Soni <s.soni.shivansh@gmail.com>
2025-09-01 15:11:36 +0530

    Rate-Limiter-Service project setup

---
+++ rate-limiter-service/src/main/java/com/example/ratelimiterservice/RateLimiterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratelimiterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimiterServiceApplication.class, args);
+    }
+
+}
---
+++ rate-limiter-service/src/test/java/com/example/ratelimiterservice/RateLimiterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratelimiterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
